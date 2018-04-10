579dd75d27c3837f724cd382536660e9f85251ce
NTDLS <josh@ntdls.com>
2018-04-09 21:17:01 -0400

    Add files via upload

---
+++ Resources/resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SystemInfo.rc
+//
+#define IDS_TITLECAPTION                1
+#define IDS_FILEVERSION                 2
+#define IDS_APPCOPYRIGHT                3
+#define IDD_MAIN                        100
+#define ID_ABOUT                        100
+#define IDC_ABOUT                       100
+#define IDI_MAINICON                    101
+#define IDI_MAIN                        101
+#define ID_CLOSE                        101
+#define IDC_CLOSE                       101
+#define ID_REFRESH                      102
+#define IDC_REFRESH                     102
+#define ID_SAVE                         103
+#define IDC_SAVE                        103
+#define ID_PRINT                        104
+#define IDC_PRINT                       104
+#define IDR_MENU1                       104
+#define IDR_MAINMENU                    104
+#define IDC_HANDCURSOR                  110
+#define IDD_NETLOGO                     146
+#define IDC_TEXTBOX                     1000
+#define ID_INFOLIST                     1000
+#define IDC_INFOLIST                    1000
+#define IDC_NETLINK                     1000
+#define IDC_OK                          1001
+#define IDC_CANCEL                      1002
+#define IDC_STATICTEXT                  1003
+#define IDC_OPTLIST                     1004
+#define IDC_ABOUTICON                   1004
+#define IDC_APPNAME                     1005
+#define IDC_APPVERSION                  1006
+#define IDC_APPCOPYRIGHT                1007
+#define IDC_OKBUTTON                    1008
+#define IDC_BUILDTIMEDATE               1009
+#define ID_HELP_ABOUT                   40004
+#define ID_ADVANCED                     40005
+#define ID_FILE_CLOSE_APP               40006
+#define ID_FILE_SAVE_INFO               40007
+#define ID_FILE_PRINT_INFO              40008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
